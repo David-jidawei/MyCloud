c2a70157c98ec8dde3d0933b0f5a135056a3f58e
Jdw <2073349972@qq.com>
2021-04-11 15:15:16 +0800

    第一次上传

--- src/main/java/com/bjpowernode/controller/OneServlet.java
+++ src/main/java/com/bjpowernode/controller/OneServlet.java
@@ -10,9 +10,6 @@ import java.io.PrintWriter;
 public class OneServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("servlet执行");
-        System.out.println("myGithub");
-        System.out.println("菜鸟不及时拉取就更新，产生冲突");
-        System.out.println("冲突");
         String name = request.getParameter("name");
         request.setAttribute("name",name);
         request.getRequestDispatcher("/result.jsp").forward(request,response);
