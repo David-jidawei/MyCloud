efad0ebd4001ddf525ee7ec7f28a1c97be3f5291
Jdw <2073349972@qq.com>
2021-04-11 14:31:56 +0800

    第一次上传

--- src/main/java/com/bjpowernode/controller/OneServlet.java
+++ src/main/java/com/bjpowernode/controller/OneServlet.java
@@ -11,6 +11,7 @@ public class OneServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("servlet执行");
         System.out.println("myGithub");
+        System.out.println("冲突");
         String name = request.getParameter("name");
         request.setAttribute("name",name);
         request.getRequestDispatcher("/result.jsp").forward(request,response);
